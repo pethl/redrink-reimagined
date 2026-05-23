a202d3c90bc8fbe58b3492e07fa503f329578af7
pethl <pethicklisa@gmail.com>
2026-05-23 13:50:43 +0100

    Remove basename from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-     <BrowserRouter basename="/redrink-reimagined">
+    <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/why-reusables" element={<WhyReusables />} />
