655932c2260657450b4bac5ef969609dc7dd6364
pethl <pethicklisa@gmail.com>
2026-05-23 13:45:48 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/redrink-reimagined/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
